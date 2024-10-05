939f1cd800dc782c901945ddbea00f6d6cdf0558
AmirrezaRezaeii <amirrezarezaeijavidan@gmail.com>
2024-10-05 13:29:37 +0000

    initial commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-3xl font-bold">welcome to my dummy blog with app router</h1>
+    </div>
+  );
+}
